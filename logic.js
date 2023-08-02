d017ec52dbe0195b75e9babff21c4be251fd5195
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-02 13:04:58 +0500

    Updated

--- logic.js
+++ logic.js
@@ -8,16 +8,11 @@ icon.addEventListener('click' , ()=>{
         pass.type = "text";
         icon.classList.remove('fa-eye-slash');
         icon.classList.add('fa-eye');
-
-
     }
     else {
         
         pass.type = "password";
         icon.classList.remove('fa-eye');
         icon.classList.add('fa-eye-slash');
-
     }
-
-
-})
+});
